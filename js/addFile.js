6ea449fe33a3f8a9fc45f4768833f407bc3befe7
9WiSHao <825763548@qq.com>
2023-01-22 18:13:04 +0800

    feat:首页侧边样式

--- js/addFile.js
+++ js/addFile.js
@@ -1,4 +1,4 @@
-let cssFile = ['./css/mainPage/mainPage.css', './css/mainPage/mainPageHeader.css'];
+let cssFile = ['./css/mainPage/mainPage.css', './css/mainPage/mainPageHeader.css', './css/mainPage/mainPageLeft.css', './css/mainPage/mainPageFooter.css'];
 let jsFile = ['./js/mainPage/mainPage.js'];
 
 let head = document.querySelector('head');
@@ -8,6 +8,8 @@ function addFile() {
 	for (let i = 0; i < cssFile.length; i++) {
 		let cssFileText = `<link rel="stylesheet" href="${cssFile[i]}">`;
 		head.insertAdjacentHTML('beforeEnd', cssFileText);
+	}
+	for (let i = 0; i < jsFile.length; i++) {
 		let jsFileText = `<script src="${jsFile[i]}" type="module"></script>`;
 		body.insertAdjacentHTML('beforeEnd', jsFileText);
 	}
